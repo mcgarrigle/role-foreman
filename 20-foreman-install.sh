382252909d0a715cb02c218e5e9ae88f79cae447
Peter McGarrigle <peter.mcgarrigle@me.com>
2017-07-12 09:49:46 +0000

    fixed puppetdb plugin

--- 20-foreman-install.sh
+++ 20-foreman-install.sh
@@ -9,6 +9,9 @@ yum install -y firewalld vim
 
 echo "DHCP/PXE interface = ${ETH0}"
 
+# --------------------------------------------------------------------------
+# enable firewall
+
 echo "Setting up Firewall Rules..."
 
 systemctl start firewalld
@@ -29,6 +32,9 @@ yum install -y http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install -y https://yum.theforeman.org/releases/1.14/el7/x86_64/foreman-release.rpm
 yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
 
+# --------------------------------------------------------------------------
+# postgres server
+
 yum install -y postgresql96-server
 
 /usr/pgsql-9.6/bin/postgresql96-setup initdb
@@ -43,6 +49,9 @@ systemctl restart postgresql-9.6
 
 yum install -y centos-release-scl centos-release-scl-rh
 # yum install -y puppetserver puppetdb puppet-agent
+
+# --------------------------------------------------------------------------
+# foreman server
 
 echo "Running Foreman Installer..."
 
