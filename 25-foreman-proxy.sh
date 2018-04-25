97ceb3360deffcc9f238ddf157574102d8970dfe
Peter McGarrigle <peter.mcgarrigle@bt.com>
2018-04-25 11:02:43 +0000

    25-foreman-proxy.sh now uses facter

--- 25-foreman-proxy.sh
+++ 25-foreman-proxy.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-source 00-environment.sh
-
 function yaml {
   TEXT=$(cat $1 | sed 's/^--- //')
   echo $TEXT
 }
+
+NAMESERVERS="10.0.30.11"
+FQDN=$(facter fqdn)
+DOMAIN=$(facter domain)
+REALM=$(uppercase ${DOMAIN})
+
+ETHS=$(facter interfaces)
+ETH0=$(echo $ETHS | cut -d, -f 1)
+ETH1=$(echo $ETHS | cut -d, -f 2)
+ADDRESS0=$(facter ipaddress_${ETH0})
+ADDRESS1=$(facter ipaddress_${ETH1})
+
+echo $HOSTNAME $DOMAIN $REALM
+echo $ETH0 $ADDRESS0
+echo $ETH1 $ADDRESS1
 
 CONSUMER_KEY_FILE='/opt/puppetlabs/puppet/cache/foreman_cache_data/oauth_consumer_key'
 CONSUMER_SECRET_FILE='/opt/puppetlabs/puppet/cache/foreman_cache_data/oauth_consumer_secret'
@@ -15,28 +28,18 @@ CONSUMER_SECRET=$(yaml $CONSUMER_SECRET_FILE)
 
 unset http_proxy https_proxy
 
+FOREMAN_URL="https://${FQDN}"
+
 foreman-installer \
   --enable-foreman-proxy \
   --foreman-proxy-tftp=true \
-  --foreman-proxy-tftp-servername="${FOR_ADDRESS}" \
+  --foreman-proxy-tftp-servername="${ADDRESS0}" \
   --foreman-proxy-dhcp=true \
   --foreman-proxy-dhcp-interface="${ETH0}" \
   --foreman-proxy-dhcp-gateway= \
-  --foreman-proxy-dhcp-nameservers="${FOR_ADDRESS}" \
+  --foreman-proxy-dhcp-nameservers="${NAMESERVERS}" \
   --foreman-proxy-dns=false \
-  --foreman-proxy-foreman-base-url="${FOR_URL}" \
+  --foreman-proxy-foreman-base-url="${FOREMAN_URL}" \
   --foreman-proxy-oauth-consumer-key="${CONSUMER_KEY}" \
   --foreman-proxy-oauth-consumer-secret="${CONSUMER_SECRET}"
-
-# Setup wildcard auto-signing
-# echo "*" > /etc/puppet/autosign.conf
-
-hammer subnet create --name "LOCAL" \
-  --boot-mode "Static" \
-  --network "${NETBASE}" \
-  --mask "${NETMASK}" \
-  --from "10.0.30.200" \
-  --to "10.0.30.250" \
-  --domains "${DOMAIN}" \
-  --ipam "None"
 
